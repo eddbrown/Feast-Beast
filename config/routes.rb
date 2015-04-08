19dc0ce73ce4dafb984d829c8d7f0d599f1cec3d
Yannick Brunner <yannickbrunner@gmail.com>
2015-04-08 10:14:03 +0100

    added links to sign in up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :feasts do
     resources :reviews
   end
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'feasts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
