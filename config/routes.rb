22198c629a4fac8805f029664bb58216ee2c960c
Edward Brown <edward.brown.london@gmail.com>
2015-04-10 15:56:39 +0100

    reset route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'feasts#index'
+  root to: 'feasts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
